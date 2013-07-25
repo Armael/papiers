069f2e2fe6098ade0d180c1fde887f550bd0638a
Armael <armael.gueneau@ens-lyon.fr>
2013-07-25 10:35:54 +0200

    Cleanup in config.ml: management of filepath should be now portable

--- config.ml
+++ config.ml
@@ -1,3 +1,5 @@
+let (^/) = Filename.concat
+
 let db_file =
-  let home = (try Unix.getenv "HOME" ^ "/" with Not_found -> "") in
-  home ^ ".papiers.db"
+  let home = (try Unix.getenv "HOME" with Not_found -> Filename.current_dir_name) in
+  home ^/ ".papiers.db"
