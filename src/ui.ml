9b04442c6a6c2f8ad1d08ff3ab9cf52a2c9baa58
Armael <armael.gueneau@ens-lyon.fr>
2013-10-30 21:48:52 +0100

    Add some functions to query informations

--- src/ui.ml
+++ src/ui.ml
@@ -70,3 +70,45 @@ let query_doc_infos () =
 
   title, authors, tags
 
+
+let query_sources (db_path: PathGen.t) =
+  print_string "Sources (comma separated): ";
+  read_line ()
+  |> String.nsplit ~by:","
+  |> List.map String.strip
+  |> List.map (Source.import ~check_file_exists:true db_path)
+
+let query_doc (db_path: PathGen.t) =
+  let title = query_title ()
+  and authors = query_authors ()
+  and sources = query_sources db_path
+  and tags = query_tags () in
+
+  title, authors, sources, tags
+
+let select_char (choices: (char * 'a) list) =
+  let choices' = List.map fst choices in
+  let answer = ref None in
+  let is_fst = ref true in
+
+  while !answer = None do
+    if not !is_fst then
+      print_newline ();
+
+    print_string "[";
+    iter_effect_tl (print_char % fst) (fun () -> print_char '/') choices;
+    print_string "] ? ";
+    Printf.printf "%!";
+
+    let c = (input_line stdin).[0] in
+    if List.mem c choices' then
+      answer := Some c
+  done;
+  List.assoc (Option.get !answer) choices
+
+let query_multi_choices (choices: (char * string * 'a) list) =
+  List.iter (fun (c, explain, _) ->
+    Printf.printf "[%c] %s\n%!" c explain
+  ) choices;
+
+  select_char (List.map Tuple3.get13 choices)
