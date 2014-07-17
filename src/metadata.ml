f55535030804bd1dd28c5d53ec0c84739faded61
Armael <armael.gueneau@ens-lyon.fr>
2014-07-17 19:16:45 +0100

    Do not crash when trying to get metadata from invalid PDFs

--- src/metadata.ml
+++ src/metadata.ml
@@ -16,12 +16,12 @@ type kind = [
 | `Lang
 ]
 
-module Pdf = struct
+module MPdf = struct
   let get_metadata (filename: string) (k: kind) =
     let cin = open_in filename |> BatIO.to_input_channel in
     let pdf_in = Pdfio.input_of_channel cin in
     let pdf = Pdfread.pdf_of_input_lazy None None pdf_in in
-    
+
     match k with
     | `Title -> Cpdf.get_info_utf8 pdf "/Title"
     | `Authors -> Cpdf.get_info_utf8 pdf "/Author"
@@ -40,7 +40,10 @@ let get (db: BatPathGen.OfString.t) (src: Source.t) (k: kind) =
   | Source.File f ->
     Option.bind (BatPathGen.OfString.ext f) (function
     | "pdf" ->
-      let d = Pdf.get_metadata (Source.export db src) k in
-      if d <> "" then Some d else None
+      begin try
+        let d = MPdf.get_metadata (Source.export db src) k in
+        if d <> "" then Some d else None
+      with (Pdf.PDFError _) -> None
+      end
     | _ -> None)
   | Source.Other _ -> None
